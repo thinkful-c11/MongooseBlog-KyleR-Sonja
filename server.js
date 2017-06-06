b6458e19fef5a08eef8bb4c12674f3effcbb62dc
Sonja Duric <duric.sonja@gmail.com>
2017-06-06 16:07:39 -0400

    all endpoints finished

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ app.get('/posts', (req, res) => {
                 post => post.apiRepr()
             )
         });
-    });
+    })
+     .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Internal server error'})
+        });
 });
 
 app.get('/posts/:id', (req, res) => {
@@ -41,13 +45,79 @@ app.get('/posts/:id', (req, res) => {
     .then(post => res.json(post.apiRepr()))
     .catch(err => {
         console.error(err);
-        res.status(500).json({message: "HOLY SHIT BATMAN!!"});
+        res.status(500).json({message: "Internal server error"});
     });
 });
 
+app.post('/posts', (req, res) => {
+
+  const requiredFields = ['title', 'author', 'content'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Blog
+    .create({
+      title: req.body.title,
+      author: req.body.author,
+      content: req.body.content,
+    })
+    .then(
+      post => res.status(201).json(post.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+app.put('/posts/:id', (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+    .exec()
+    .then(post => res.status(201).json(post.apiRepr()))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
 
 
+app.delete('/posts/:id', (req, res) => {
+  Blog
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
 
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
 
 
 
